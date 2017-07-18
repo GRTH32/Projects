8523b6010aaa40bcc5b550a9336d2222ba87a56b
Евгений Акулов < >
2017-07-18 14:09:16 +1000

    Добавьте файлы проекта.

---
+++ leveler/leveler.cpp
@@ -0,0 +1,10 @@
+﻿/*
+ Name:		leveler.cpp
+ Created:	18.07.2017 14:08:44
+ Author:	Евгений
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "leveler.h"
+
+
---
+++ leveler/leveler.h
@@ -0,0 +1,19 @@
+﻿/*
+ Name:		leveler.h
+ Created:	18.07.2017 14:08:44
+ Author:	Евгений
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _leveler_h
+#define _leveler_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
